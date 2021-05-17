6231677594b9f4367302274e855e338287e0b864
labyseson <lbesf.chn@gmail.com>
2021-05-17 16:52:26 -0700

    added block group level code

--- get_acs_data.R
+++ get_acs_data.R
@@ -17,6 +17,7 @@ library(tidycensus)
 # acs_vars_p <- load_variables(2019,"acs5/profile",cache=TRUE)
 # acs_vars_s <- load_variables(2019,"acs5/subject",cache=TRUE)
 
+#tract level
 vars <- c("B06007_001",#population (age 5+) for ability to speak english
           "B06007_005",#Spanish & limited English
           "B06007_008",#Other languages & limited English
@@ -32,32 +33,83 @@ vars <- c("B06007_001",#population (age 5+) for ability to speak english
           "B03002_005"#American Indian and Alaska Native
           )
 
-vars_acs <- get_acs(geography = "tract",
+vars_acs_ct <- get_acs(geography = "tract",
                         variables = vars,
                         state = "CA",
+                        county = "Los Angeles County",
                         year = 2019) %>%
-            filter(substr(GEOID,3,5) == "037") %>%
-            select(-c(NAME, moe)) %>%
-            pivot_wider(names_from = variable, values_from = estimate) %>%
-            mutate(Limited_Eng_per = (B06007_005 + B06007_008) / B06007_001 * 100,
-                   Poverty_per = B06012_002 / B06012_001 * 100, 
-                   NonHisp_asian_per = B03002_006/B03002_001 * 100,
-                   NonHisp_pi_per = B03002_007/B03002_001 * 100,
-                   NonHisp_black_per = B03002_004/B03002_001 * 100,
-                   NonHisp_white_per = B03002_003/B03002_001 * 100,
-                   Hisp_per = B03002_012/B03002_001 * 100,
-                   NonHisp_ai_per = B03002_005/B03002_001 * 100
-            ) %>% 
-            rename(Uninsured_per = S2701_C05_001) %>% 
-            select(GEOID, 
-                   Limited_Eng_per,
-                   Poverty_per, 
-                   Uninsured_per,
-                   NonHisp_asian_per,
-                   NonHisp_pi_per,
-                   NonHisp_black_per,
-                   NonHisp_white_per,
-                   Hisp_per,
-                   NonHisp_ai_per)
+               select(-c(NAME, moe)) %>%
+               pivot_wider(names_from = variable, values_from = estimate) %>%
+               mutate(Limited_Eng_per = (B06007_005 + B06007_008) / B06007_001 * 100,
+                      Poverty_per = B06012_002 / B06012_001 * 100, 
+                      NonHisp_asian_per = B03002_006/B03002_001 * 100,
+                      NonHisp_pi_per = B03002_007/B03002_001 * 100,
+                      NonHisp_black_per = B03002_004/B03002_001 * 100,
+                      NonHisp_white_per = B03002_003/B03002_001 * 100,
+                      Hisp_per = B03002_012/B03002_001 * 100,
+                      NonHisp_ai_per = B03002_005/B03002_001 * 100
+               ) %>% 
+               rename(Uninsured_per = S2701_C05_001) %>% 
+               select(GEOID, 
+                      Limited_Eng_per,
+                      Poverty_per, 
+                      Uninsured_per,
+                      NonHisp_asian_per,
+                      NonHisp_pi_per,
+                      NonHisp_black_per,
+                      NonHisp_white_per,
+                      Hisp_per,
+                      NonHisp_ai_per)
 
-write.csv(vars_acs, "./acs_vars_results.csv")
+write.csv(vars_acs_ct, "./acs_vars_results_tracts.csv")
+
+#block group level
+vars1 <- c("B06007_001",#population (age 5+) for ability to speak english
+           "B06007_005",#Spanish & limited English
+           "B06007_008",#Other languages & limited English
+           "B06012_001",#population for whom poverty status is determined
+           "B06012_002",#Below 100 percent of the poverty level
+           "S2701_C05_001",#percent uninsured
+           "B03002_001",#total population
+           "B03002_006",#Asian
+           "B03002_007",#Native Hawaiian and Other PI
+           "B03002_004",#Black
+           "B03002_003",#Non-Hispanic White
+           "B03002_012",#Hispanic or Latino 
+           "B03002_005"#American Indian and Alaska Native
+           )
+vars_acs_bg <- get_acs(geography = "block group",
+                       variables = "B03002_006",
+                       state = "CA",
+                       county = "Los Angeles County",
+                       year = 2019) %>%
+               #filter(substr(GEOID,3,5) == "037") %>%
+               select(-c(NAME, moe)) %>%
+               pivot_wider(names_from = variable, values_from = estimate) %>%
+               mutate(Limited_Eng_per = (B06007_005 + B06007_008) / B06007_001 * 100,
+                      Poverty_per = B06012_002 / B06012_001 * 100, 
+                      NonHisp_asian_per = B03002_006/B03002_001 * 100,
+                      NonHisp_pi_per = B03002_007/B03002_001 * 100,
+                      NonHisp_black_per = B03002_004/B03002_001 * 100,
+                      NonHisp_white_per = B03002_003/B03002_001 * 100,
+                      Hisp_per = B03002_012/B03002_001 * 100,
+                      NonHisp_ai_per = B03002_005/B03002_001 * 100
+               ) %>% 
+               rename(Uninsured_per = S2701_C05_001) %>% 
+               select(GEOID, 
+                      Limited_Eng_per,
+                      Poverty_per, 
+                      Uninsured_per,
+                      NonHisp_asian_per,
+                      NonHisp_pi_per,
+                      NonHisp_black_per,
+                      NonHisp_white_per,
+                      Hisp_per,
+                      NonHisp_ai_per)
+
+write.csv(vars_acs_bg, "./acs_vars_results_blockgroups.csv")
+
+
+
+
+
